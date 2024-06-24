5cc5a8549daedc90b60564e45409665121b282a7
Jeslyn Chang <121521414+se1yu@users.noreply.github.com>
2024-06-24 21:30:26 +0000

    made boxes on bottom of site +format images

--- components/section2.tsx
+++ components/section2.tsx
@@ -7,7 +7,7 @@ import shell from "../assets/shell.svg";
 const Section2 = () => {
   return (
     <div className="bg-[url('/sand.svg')] sm:h-[100vh] h-[150vh] bg-cover ">
-      <div className="sm:flex items-center justify-center gap-[100px] mt-20">
+      <div className="sm:flex items-center justify-center gap-[100px]">
         <div className="bg-[#FFEAC7] sm:w-[500px] py-12rem rounded-[12px] h-[35vh] overflow-hidden flex flex-col justify-center">
           <div className="text-[#E17E23] text-[32px] league text-center">
             What is a hackathon?
@@ -23,23 +23,24 @@ const Section2 = () => {
           <Image src={pinkCoral} alt="pinkCoral" fetchPriority="high" className="absolute z-10 left-[55px] pt-[5rem]" />
         </div>
 
-        <div className="bg-[#FFEAC7] sm:w-[500px] rounded-[12px] h-[30.12vh] py-10 overflow-hidden flex flex-col justify-center mt-10">
+        <div className="bg-[#FFEAC7] sm:w-[500px] rounded-[12px] h-[35vh] py-10 overflow-hidden flex flex-col justify-center">
           <div className="text-[#E17E23] text-[32px] league text-center">
             Where?
           </div>
           <div className="league text-center text-[#F4B349] text-[30px]">
-            Sandy Port, Bahamas!
+            <a href="https://maps.app.goo.gl/T4pNj8n2wBy7aQG47" className ="underline">
+            Sandy Port, Bahamas!  </a>
             <br />
             July 17th, 2024.
           </div>
-          <Image src={seaweed} alt="seaweed" fetchPriority="high" />
+          <Image src={seaweed} alt="seaweed" fetchPriority="high" className="absolute z-10 right-[50px] pt-[8rem]"/>
         </div>
       </div>
 
       <div className="text-center flex justify-center sm:pt-10"></div>
-      <div className="sm:flex items-center justify-center gap-[100px] mt-10">
+      <div className="sm:flex items-center justify-center gap-[100px]">
         {/********** a box **********/}
-        <div className="bg-[#FFEAC7] sm:w-[500px] rounded-[12px] py-10 h-[50vh] overflow-hidden flex flex-col justify-center">
+        <div className="bg-[#FFEAC7] sm:w-[500px] rounded-[12px] py-10 h-[35vh] overflow-hidden flex flex-col justify-center">
           <div className="text-[#E17E23] text-[32px] league text-center">
             How much does it cost?
           </div>
@@ -47,25 +48,26 @@ const Section2 = () => {
             It's free! Email <br /> ivoine@hackthebeach.dev <br /> for any
             questions/inquiries.
           </div>
-          <Image src={shell} alt="shell" fetchPriority="high" />
+          <Image src={shell} alt="shell" fetchPriority="high"className="absolute z-10 left-[55px] pt-[5rem]" />
         </div>
 
-        <div className="bg-[#FFEAC7] sm:w-[500px] rounded-[12px] h-[30.12vh] py-10 overflow-hidden flex flex-col justify-center mt-10">
-          <div className="text-[#E17E23] text-[32px] league text-center">
-            Build a boat!
+        <div className="bg-[#FFEAC7] sm:w-[500px] rounded-[12px] h-[35vh] py-10 overflow-hidden flex flex-col justify-center">
+          <div className="text-[#E17E23] text-[40px] league text-center">
+            
+            Build a boat! 
           </div>
-          <div className="league text-center text-[#F4B349] text-[18px]">
-            Sandy Port, Bahamas!
-            <br />
-            July 17th, 2024.
+          <div className="league text-center text-[#F4B349] text-[30px]">
+            Step 1: Cardboard. <br/>
+            Step 2: Duct tape.<br/>
+            Step 3; Float on water.
           </div> 
-          <Image src={flower} alt="flower" fetchPriority="high" />
+          <Image src={flower} alt="flower" fetchPriority="high"className="absolute z-10 right-[50px] pt-[0rem]" />
         </div>
 
         
       </div>
-      <div className="text-center flex justify-center">
-        <Image src={flowers} alt="flowers" fetchPriority="high" />
+      <div className="text-center flex justify-center mt-5">
+        <Image src={flowers} alt="flowers" fetchPriority="high "/>
       </div>
     </div>
   );
